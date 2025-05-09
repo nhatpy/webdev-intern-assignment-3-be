3a00aeba09d9e72024d75287b8e32baf838e2d34
788465h10 <dolongnhat0301@gmail.com>
2025-05-09 10:58:55 +0700

    init

---
+++ src/main/java/com/g_scrores/g_scores/GScoresApplication.java
@@ -0,0 +1,13 @@
+package com.g_scrores.g_scores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GScoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GScoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/g_scrores/g_scores/GScoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g_scrores.g_scores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GScoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
