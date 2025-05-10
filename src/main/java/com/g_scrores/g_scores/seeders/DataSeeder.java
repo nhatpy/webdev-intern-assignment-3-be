a1ef7d7c21e05ce5c5a5e8444a9ba059dd3fae9e
788465h10 <dolongnhat0301@gmail.com>
2025-05-10 20:40:10 +0700

    update dataseeder with threah

--- src/main/java/com/g_scrores/g_scores/seeders/DataSeeder.java
+++ src/main/java/com/g_scrores/g_scores/seeders/DataSeeder.java
@@ -1,14 +1,12 @@
 package com.g_scrores.g_scores.seeders;
 
 import com.g_scrores.g_scores.models.ScoreEntity;
-import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -20,12 +18,16 @@ public class DataSeeder implements CommandLineRunner {
 
     private final JdbcTemplate jdbcTemplate;
 
-    private static final int BATCH_SIZE = 2000;
+    private static final int BATCH_SIZE = 10000;
 
     @Override
     public void run(String... args) throws Exception {
         if (isDatabaseEmpty()) {
-            loadDataFromCSV();
+            jdbcTemplate.execute("SET FOREIGN_KEY_CHECKS = 0");
+            jdbcTemplate.execute("ALTER TABLE scores DISABLE KEYS");
+            loadDataWithMultithreading();
+            jdbcTemplate.execute("SET FOREIGN_KEY_CHECKS = 1");
+            jdbcTemplate.execute("ALTER TABLE scores ENABLE KEYS");
         } else {
             System.out.println("Database already seeded with data from 'diem_thi_thpt_2024.csv'");
         }
@@ -35,53 +37,67 @@ public class DataSeeder implements CommandLineRunner {
         return jdbcTemplate.queryForObject("SELECT COUNT(*) FROM scores", Integer.class) == 0;
     }
 
-    private void loadDataFromCSV() {
+    private void loadDataWithMultithreading() {
         try (InputStream is = getClass().getClassLoader().getResourceAsStream("data/diem_thi_thpt_2024.csv")) {
-            if (is == null) {
-                throw new FileNotFoundException("Seeder file not found");
-            }
-
-            InputStreamReader reader = new InputStreamReader(is);
-            CsvToBean<ScoreEntity> csvToBean = new CsvToBeanBuilder<ScoreEntity>(reader)
+            List<ScoreEntity> scoreEntities = new CsvToBeanBuilder<ScoreEntity>(new InputStreamReader(is))
                     .withType(ScoreEntity.class)
                     .withIgnoreLeadingWhiteSpace(true)
-                    .build();
+                    .build()
+                    .parse();
 
-            List<ScoreEntity> scoreEntities = csvToBean.parse();
+            int processors = Runtime.getRuntime().availableProcessors();
+            int chunkSize = scoreEntities.size() / processors;
 
-            String sql = "INSERT INTO scores (sbd, toan, ngu_van, ngoai_ngu, vat_li, hoa_hoc, sinh_hoc, lich_su, dia_li, gdcd, ma_ngoai_ngu) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            List<Thread> threads = new ArrayList<>();
 
-            List<Object[]> batchArgs = new ArrayList<>();
-            int count = 0;
+            for (int i = 0; i < processors; i++) {
+                final int startIdx = i * chunkSize;
+                final int endIdx = (i == processors - 1) ? scoreEntities.size() : (i + 1) * chunkSize;
 
-            for (ScoreEntity scoreEntity : scoreEntities) {
-                Object[] args = new Object[] {
-                        scoreEntity.getSbd(),
-                        scoreEntity.getToan(),
-                        scoreEntity.getNgu_van(),
-                        scoreEntity.getNgoai_ngu(),
-                        scoreEntity.getVat_li(),
-                        scoreEntity.getHoa_hoc(),
-                        scoreEntity.getSinh_hoc(),
-                        scoreEntity.getLich_su(),
-                        scoreEntity.getDia_li(),
-                        scoreEntity.getGdcd(),
-                        scoreEntity.getMa_ngoai_ngu()
-                };
-                batchArgs.add(args);
+                Thread thread = new Thread(() -> {
+                    insertBatch(scoreEntities.subList(startIdx, endIdx));
+                });
 
-                if (++count % BATCH_SIZE == 0) {
-                    jdbcTemplate.batchUpdate(sql, batchArgs);
-                    batchArgs.clear();
-                }
+                threads.add(thread);
+                thread.start();
             }
 
-            if (!batchArgs.isEmpty()) {
-                jdbcTemplate.batchUpdate(sql, batchArgs);
+            for (Thread thread : threads) {
+                thread.join();
             }
-
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    private void insertBatch(List<ScoreEntity> entities) {
+        String sql = "INSERT INTO scores VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        List<Object[]> batchArgs = new ArrayList<>();
+
+        for (ScoreEntity scoreEntity : entities) {
+            Object[] args = new Object[] {
+                    scoreEntity.getSbd(),
+                    scoreEntity.getToan(),
+                    scoreEntity.getNgu_van(),
+                    scoreEntity.getNgoai_ngu(),
+                    scoreEntity.getVat_li(),
+                    scoreEntity.getHoa_hoc(),
+                    scoreEntity.getSinh_hoc(),
+                    scoreEntity.getLich_su(),
+                    scoreEntity.getDia_li(),
+                    scoreEntity.getGdcd(),
+                    scoreEntity.getMa_ngoai_ngu()
+            };
+            batchArgs.add(args);
+
+            if (batchArgs.size() >= BATCH_SIZE) {
+                jdbcTemplate.batchUpdate(sql, batchArgs);
+                batchArgs.clear();
+            }
+        }
+
+        if (!batchArgs.isEmpty()) {
+            jdbcTemplate.batchUpdate(sql, batchArgs);
         }
     }
 }
